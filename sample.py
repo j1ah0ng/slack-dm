14e9bda3a0ea4058fea1a41b04a34e525636a181
sushisharkjl <jiahong888long@gmail.com>
2018-06-06 22:36:06 -0700

    Added sample.

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ with open('users', 'r') as file:
         msgs     = ["hello%20" + name[0] + "!", 
                     "hope%20you%27re%20having%20a%20nice%20summer",
                     "just%20wanted%20to%20remind%20you%20that%20our%20team%20banquet%20is%20this%20friday%20at%204%3A30pm%20at%20the%20civic%20center",
-                    "if%20you%27re%20coming%2C%20please%20rsvp%20at%20https%3A%2F%2Fdocs.google.com%2Fforms%2Fu%2F3%2Fd%2Fe%2F1FAIpQLSdECmJnDk5SoaMLF6Bd5iVokxdBR1lagvqzfgH7aSUX27r2mw%2Fviewform",
+                    "if%20you%27re%20coming%2C%20please%20rsvp%20at%20https%3A%2F%2Fredacted",
                     "let%20me%20know%20if%20you%20have%20any%20questions"
                     ]
         user     = "firstname.lastname"
